8c609c96e049c99277b7283cc6933ef657867f62
yn5 <ynsnetmail@gmail.com>
2018-07-28 22:32:34 +0200

    Print coordinates of first LineString in input GeoJSON

--- __main__.py
+++ __main__.py
@@ -1,1 +1,13 @@
+import sys
+import geopandas
 
+inputFilePath = str(sys.argv[1])
+dataFrameFroms = geopandas.read_file(inputFilePath)
+dataFrameTos = geopandas.read_file(inputFilePath)
+
+print(inputFilePath)
+
+line = dataFrameFroms['geometry'][0]
+lineCoords = list(list(line.geoms)[0].coords)
+
+print(lineCoords)
