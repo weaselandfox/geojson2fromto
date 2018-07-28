743c978d8bf826e27dd4b0a3a33451575443e081
Bram Kok <bram@bramkok.com>
2018-07-29 01:02:54 +0200

    Coordinate tuples to JSON

--- __main__.py
+++ __main__.py
@@ -1,13 +1,25 @@
 import sys
+import json
 import geopandas
 
 inputFilePath = str(sys.argv[1])
 dataFrameFroms = geopandas.read_file(inputFilePath)
 dataFrameTos = geopandas.read_file(inputFilePath)
 
-print(inputFilePath)
-
 line = dataFrameFroms['geometry'][0]
-lineCoords = list(list(line.geoms)[0].coords)
+lineCoordinatesList = list(list(line.geoms)[0].coords)
 
-print(lineCoords)
+lineCoordinatesListA = lineCoordinatesList[:-1]
+lineCoordinatesListB = lineCoordinatesList[1:]
+
+i = 0
+d = {}
+a = []
+
+for i in range(len(lineCoordinatesListA)):
+    d['from'] = lineCoordinatesListA[i]
+    d['to'] = lineCoordinatesListB[i]
+
+    a.append(d)
+
+print(json.dumps(a))
