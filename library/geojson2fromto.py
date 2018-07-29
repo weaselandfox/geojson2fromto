6b4abfc4aa26c9a6ee7c9af34b390c675cac9d6f
yn5 <ynsnetmail@gmail.com>
2018-07-29 16:45:34 +0200

    Refactor geojson2fromto#convert

--- library/geojson2fromto.py
+++ library/geojson2fromto.py
@@ -4,26 +4,45 @@ Module for converting geojson to from-to json.
 
 import geopandas
 
-def convert(input_file_path):
+def get_geometries(input_file_path):
     """
-    Converts a GeoJSON file to a list with dictionaries containing from and to fields.
+    Reads a GeoJSON input file and ouputs a geopandas.GeoSeries object
+    with geometries.
     """
+
     data_frame = geopandas.read_file(input_file_path)
 
-    line = data_frame['geometry'][0]
-    line_coordinates_list = list(list(line.geoms)[0].coords)
+    return data_frame['geometry']
 
-    line_coordinates_list_a = line_coordinates_list[:-1]
-    line_coordinates_list_b = line_coordinates_list[1:]
+def get_fromto(linestring_coords):
+    """
+    Converts a list of linestring coordinates to a list of 'from-to'
+    dictionaries.
+    """
+    line_coordinates_list_froms = linestring_coords[:-1]
+    line_coordinates_list_tos = linestring_coords[1:]
 
     i = 0
     fromtos = []
 
-    for i, val in enumerate(line_coordinates_list_a):
+    for i, val in enumerate(line_coordinates_list_froms):
         fromto_dict = {}
         fromto_dict['from'] = val
-        fromto_dict['to'] = line_coordinates_list_b[i]
+        fromto_dict['to'] = line_coordinates_list_tos[i]
 
         fromtos.append(fromto_dict)
 
     return fromtos
+
+def convert(input_file_path):
+    """
+    Converts a GeoJSON file to a list with dictionaries containing from
+    and to fields.
+    """
+
+    geometries = get_geometries(input_file_path)
+
+    multi_linestring = geometries[0]
+    linestring_coords = list(list(multi_linestring.geoms)[0].coords)
+
+    return get_fromto(linestring_coords)
--- tests/test_geojson2fromto.py
+++ tests/test_geojson2fromto.py
@@ -10,7 +10,9 @@ from library import geojson2fromto
 
 @pytest.fixture()
 def generate_convert_parameters():
-    """Fixture function to generate fixtures for the convert test"""
+    """
+    Fixture function to generate fixtures for the convert test.
+    """
     test_input = os.path.abspath(os.path.join(__file__, os.pardir) + '/fixtures/roads.geojson')
 
     expected_output = [{
@@ -27,7 +29,9 @@ def generate_convert_parameters():
     return test_input, expected_output
 
 def test_convert(generate_convert_parameters):
-    """It should convert input geojson to 'from-to' json"""
+    """
+    It should convert input geojson to 'from-to' json.
+    """
     test_input = generate_convert_parameters[0]
     expected_output = generate_convert_parameters[1]
     assert geojson2fromto.convert(test_input) == expected_output
