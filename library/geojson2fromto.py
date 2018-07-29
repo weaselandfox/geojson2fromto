38ca7be1795e5ddc448823f4a021ec510a00030a
yn5 <ynsnetmail@gmail.com>
2018-07-29 18:02:00 +0200

    Add support for multi linestrings

--- library/geojson2fromto.py
+++ library/geojson2fromto.py
@@ -2,6 +2,7 @@
 Module for converting geojson to from-to json.
 """
 
+from functools import reduce
 import geopandas
 
 def get_geometries(input_file_path):
@@ -14,7 +15,7 @@ def get_geometries(input_file_path):
 
     return data_frame['geometry']
 
-def get_fromto(linestring_coords):
+def get_fromtos(linestring_coords):
     """
     Converts a list of linestring coordinates to a list of 'from-to'
     dictionaries.
@@ -34,6 +35,17 @@ def get_fromto(linestring_coords):
 
     return fromtos
 
+def get_fromtos_multi(multi_linestring_coords):
+    """
+    Converts a list of lists containing linestring coordinates to a
+    list of 'from-to' dictionaries.
+    """
+    return reduce(
+        (lambda fromtos, linestring: fromtos + get_fromtos(linestring)),
+        multi_linestring_coords,
+        []
+    )
+
 def convert(input_file_path):
     """
     Converts a GeoJSON file to a list with dictionaries containing from
@@ -43,6 +55,9 @@ def convert(input_file_path):
     geometries = get_geometries(input_file_path)
 
     multi_linestring = geometries[0]
-    linestring_coords = list(list(multi_linestring.geoms)[0].coords)
+    multi_linestring_coords = list(map(
+        lambda linestring: list(linestring.coords),
+        list(multi_linestring.geoms)
+    ))
 
-    return get_fromto(linestring_coords)
+    return get_fromtos_multi(multi_linestring_coords)
--- tests/test_geojson2fromto.py
+++ tests/test_geojson2fromto.py
@@ -9,9 +9,9 @@ import pytest
 from library import geojson2fromto
 
 @pytest.fixture()
-def generate_convert_parameters():
+def convert_parameters():
     """
-    Fixture function to generate fixtures for the convert test.
+    Fixture function.
     """
     test_input = os.path.abspath(os.path.join(__file__, os.pardir) + '/fixtures/roads.geojson')
 
@@ -28,10 +28,49 @@ def generate_convert_parameters():
 
     return test_input, expected_output
 
-def test_convert(generate_convert_parameters):
+@pytest.fixture()
+def convert_multi_parameters():
+    """
+    Fixture function.
+    """
+    test_input = os.path.abspath(
+        os.path.join(__file__, os.pardir) + '/fixtures/roads_multiline.geojson'
+    )
+
+    expected_output = [{
+        'from': (5.29304565210666, 51.677748170157187),
+        'to': (5.293036226696422, 51.67776112527045)
+    }, {
+        'from': (5.293036226696422, 51.67776112527045),
+        'to': (5.29302863902825, 51.677771119362951)
+    }, {
+        'from': (5.29302863902825, 51.677771119362951),
+        'to': (5.293018976918102, 51.677782224485654)
+    }, {
+        'from': (5.293018976918102, 51.677782224485654),
+        'to': (5.293006100024401, 51.677792310480484)
+    }, {
+        'from': (5.293006100024401, 51.677792310480484),
+        'to': (5.292990286295295, 51.677801836140205)
+    }, {
+        'from': (5.292990286295295, 51.677801836140205),
+        'to': (5.292980854535434, 51.677806353823691)
+    }]
+
+    return test_input, expected_output
+
+def test_convert(convert_parameters):
     """
     It should convert input geojson to 'from-to' json.
     """
-    test_input = generate_convert_parameters[0]
-    expected_output = generate_convert_parameters[1]
+    test_input = convert_parameters[0]
+    expected_output = convert_parameters[1]
+    assert geojson2fromto.convert(test_input) == expected_output
+
+def test_convert_multi(convert_multi_parameters):
+    """
+    It should concatenate all linestrings in a multi linestring.
+    """
+    test_input = convert_multi_parameters[0]
+    expected_output = convert_multi_parameters[1]
     assert geojson2fromto.convert(test_input) == expected_output
